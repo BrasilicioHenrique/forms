c94ea20d020ab7550deca50543c58f0c94fd0ce8
BrasilicioHenrique <brasiliciohcr@gmail.com>
2024-12-25 16:11:55 -0300

    add comentários

--- js/verificar.js
+++ js/verificar.js
@@ -1,14 +1,14 @@
 function verificarRespostas() {
+    // criando variáveis com os valores escritos no forms
     const nome = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const nasc = document.getElementById("nasc").value;
 
-    if (!nome || !email || !nasc) {
-        alert("Todos os campos devem ser preenchidos!");
-        return false;
+    if (!nome || !email || !nasc) { // verifica se o valor de nome ou o valor do email ou o valor do nasc não foram escritos
+        alert("Todos os campos devem ser preenchidos!"); // escreve se a situação acima for true
     }
     else {
-        alert("Ao continuat, você concorda em compartilhar esses dados.")
-        window.location.href = "../html/botao.html";
+        alert("Ao continuat, você concorda em compartilhar esses dados."); // escreve se a condição do if acima for false
+        window.location.href = "../html/botao.html"; // redireciona a página para botao.html
     }
 }
