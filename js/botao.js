2aad9d27f9aca96559eb3dbadc0a67d40a4c813d
BrasilicioHenrique <brasiliciohcr@gmail.com>
2025-01-02 17:53:06 -0300

    mudando num do negocio

--- js/botao.js
+++ js/botao.js
@@ -1,5 +1,5 @@
 let contador = 0; // variavel que pode mudar = 0
-const maximoCliques = 30; // variavel que nao pode mudar = 30
+const maximoCliques = 50; // variavel que nao pode mudar = 30
 
 function gerarPosicaoAleatoria() {
   const margem = 100; // define margem para nao deixar proximo da borda
@@ -33,7 +33,7 @@ function resposta(opcao) {
       botaoNao.style.top = `${posicaoAleatoria.y}px`; // define que no style: ... {top: a posicao aleatoria}
     }
 
-    if (contador === 28) { // aparecer botao "mengo"
+    if (contador === 48) { // aparecer botao "mengo"
       document.getElementById("botao_escondido").style.display = "block";
     }
   }
