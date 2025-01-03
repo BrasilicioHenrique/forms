d9cc97f4f3de9175db239c5be86f2d0ae2a6521a
BrasilicioHenrique <brasiliciohcr@gmail.com>
2025-01-02 23:32:39 -0300

    add básico

--- js/lista_nomes.js
+++ js/lista_nomes.js
@@ -1,3 +1,10 @@
 let nomesGay = [];
-
 let nomesFlamengo = [];
+
+const formulario = document.getElementById("formulario");
+const listaGay = document.getElementById("listaGay");
+const listaMengo = document.getElementById("listaMengo")
+
+function atualizarLista () {
+    
+}
